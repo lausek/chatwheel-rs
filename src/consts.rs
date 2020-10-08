c054691d3329912d902bb67ea56d50b40135405e
lausek <root@lausek.eu>
2020-10-08 21:45:32 +0200

    update default chatwheel

--- src/consts.rs
+++ src/consts.rs
@@ -5,6 +5,13 @@ pub const CHATWHEEL_DEFAULT: &[&str] = &[
     "ti2018_156", // Lakad Matataaag! Normalin, Normalin.
     "ti10_174",   // Это ГГ
     "ti2018_154", // The next level play!
+    "ti10_191", // NAKUPUU
+    "ti10_195", // I'm not angry, I'm Just Disappointed
+    "ti10_177", // Да? Да? Да? Нет.
+    "ti10_276", // Что это?! Какая жесть!
+    "ti10_275", // See you later nerds
+    "ti10_230", // Ceeeb*
+    "ti2019_184", // This guy has no chill
 ];
 pub const NAME: &str = "chatwheel-rs";
 pub const HEIGHT: u32 = 200;
