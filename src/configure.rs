de71742b5f2c02c8ce91a849d3d441fbc64a45db
lausek <root@lausek.eu>
2020-10-13 18:11:42 +0200

    style of configuration page

--- src/configure.rs
+++ src/configure.rs
@@ -5,35 +5,54 @@ use crate::settings::{create_config_file, Settings};
 fn configure_line_table() -> String {
     let settings = Settings::load().unwrap();
     let all_lines = load().unwrap();
-    let mut lis = vec![];
+    let mut all_lines = all_lines.into_iter().collect::<Vec<_>>();
 
-    for (line_id, line) in all_lines.iter() {
-        let is_configured = settings
-            .lines
-            .iter()
-            .any(|line| line.id.as_ref().unwrap() == line_id);
+    all_lines.sort_unstable_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
-        let audio_button = if let Some(audio) = line.audios.get(0) {
-            format!("<audio><source src='{}' type='audio/mpeg'></audio>", audio)
-        } else {
-            format!("")
-        };
+    let lis = all_lines
+        .iter()
+        .map(|(line_id, line)| {
+            let is_configured = settings
+                .lines
+                .iter()
+                .any(|line| line.id.as_ref().unwrap() == line_id);
 
-        let li_html = format!(
-            "<li>
-                <input type='checkbox' id='{id}' name='{id}' {checked}>
-                <label for='{id}'>{line}</label>
-                {audio}
-            </li>",
-            id = line_id,
-            line = line.text,
-            checked = if is_configured { "checked" } else { "" },
-            audio = audio_button,
-        );
-        lis.push(li_html);
-    }
+            let audio_button = if let Some(audio) = line.audios.get(0) {
+                format!(
+                    "
+                    <button type='button' onclick='this.nextElementSibling.play()'>Play</button>
+                    <audio preload='none'><source src='{}' type='audio/mpeg'></audio>
+                ",
+                    audio
+                )
+            } else {
+                format!("")
+            };
 
-    format!("<table><ul>{}</ul></table>", lis.join("\n"))
+            format!(
+                "<tr>
+                    <td class='line-checkbox'>
+                        <input type='checkbox' id='{id}' name='{id}' {checked}>
+                    </td>
+                    <td class='line-id'>
+                        {id}
+                    </td>
+                    <td class='line-audio'>
+                        {audio}
+                    </td>
+                    <td class='line-text'>
+                        <label for='{id}'>{line}</label>
+                    </td>
+                </tr>",
+                id = line_id,
+                line = line.text,
+                checked = if is_configured { "checked" } else { "" },
+                audio = audio_button,
+            )
+        })
+        .collect::<Vec<_>>();
+
+    format!("<table>{}</table>", lis.join("\n"))
 }
 
 fn configure_page() -> String {
@@ -48,6 +67,12 @@ fn configure_page() -> String {
                         top: 40px;
                         right: 40px;
                         font-size: 20px;
+                    }}
+                    tr:nth-child(odd) {{
+                        background-color: #E6E6E6;
+                    }}
+                    td {{
+                        padding: 3px;
                     }}
                 </style>
             </head>
